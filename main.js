572df14b8a1e2b7cddda6dac5fff3d1d164321b2
robiulhasanrohan <robiul@blogdesire.com>
2021-10-09 12:56:12 +0600

    fixing some bugs

--- main.js
+++ main.js
@@ -2,18 +2,16 @@
 var closeBtn = document.querySelector(".close-button")
 var settingBtn = document.querySelector(".setting-button button")
 var cdModal = document.querySelector(".cd-modal")
-settingBtn.addEventListener("click", () => {
-    cdModal.style.display = "flex";
-    cdModal.style.left = "50%";
-    cdModal.style.top = "50%";
-    cdModal.style.width = "50%";
 
+settingBtn.addEventListener("click", () => {
+    cdModal.classList.add("open-modal");
 })
 closeBtn.addEventListener("click", () => {
-    // cdModal.style.display = "none"
-    cdModal.style.left = "50%";
-    cdModal.style.top = "-50%";
-    cdModal.style.width = "0";
+    cdModal.classList.remove("open-modal");
+})
+document.addEventListener("click", (e) => {
+    if (!e.path.includes(cdModal) && !e.path.includes(settingBtn)) {
+        cdModal.classList.remove("open-modal");
 
-    console.log('click');
+    }
 })
